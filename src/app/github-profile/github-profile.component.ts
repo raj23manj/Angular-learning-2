3fdb03cab7ff69a4e0f3de8a37545319c206f685
rajesh23-drg <rmanjunath@teamdrg.com>
2018-02-02 10:19:24 +0530

    why route parameters are observable and using snapshot

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -12,13 +12,29 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.paramMap
-        .subscribe(params => {
-          console.log(params);
-          // in javascript + converts string to num
-          let id = +params.get('id');
-          console.log(id);
-        })
+
+    /*
+      if implementation like below like navigating on same page then observable should
+      be used,
+
+      this.route.paramMap
+          .subscribe(params => {
+            console.log(params);
+            // in javascript + converts string to num
+            let id = +params.get('id');
+            console.log(id);
+          });
+
+
+      if not you can use snapshot
+
+      this.route.snapshot.paramMap.get('id')
+    */
+
+    let id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+
+    console.log('GitHub Profile OnInit');
   }
 
 }
